0d291a421ac77d3940f0df0469a153ed522ce869
Paulo Ponciano <96355974+paulofponciano@users.noreply.github.com>
2023-10-12 11:38:37 -0300

    initial lab port idp

--- scripts/create-app-order.sh
+++ scripts/create-app-order.sh
@@ -2,7 +2,7 @@ NAME=$1
 PROVIDER=$2
 REGION=$3
 
-FILE_PATH=deployed_infra/${NAME}-order.yaml
+FILE_PATH=deployed_infra/${NAME}.yaml
 
 cp crossplane_compositions/order/order-claim-port.yaml $FILE_PATH
 yq --inplace ".metadata.name = \"${NAME}\"" $FILE_PATH
