3388ed93a093f7bfd87702cd845714b8c60ada17
Paulo Ponciano <96355974+paulofponciano@users.noreply.github.com>
2023-10-20 17:28:20 -0300

    lab idp port

--- scripts/create-app-order.sh
+++ scripts/create-app-order.sh
@@ -1,6 +1,8 @@
 NAME=$1
 PROVIDER=$2
 REGION=$3
+OWNER=$4
+PROJECT=$5
 
 FILE_PATH=deployed_infra/${NAME}.yaml
 
@@ -8,3 +10,5 @@ cp crossplane_compositions/order/order-claim-port.yaml $FILE_PATH
 yq --inplace ".metadata.name = \"${NAME}\"" $FILE_PATH
 yq --inplace ".spec.resourceConfig.providerConfigName = \"${PROVIDER}\"" $FILE_PATH
 yq --inplace ".spec.resourceConfig.region = \"${REGION}\"" $FILE_PATH
+yq --inplace ".spec.resourceConfig.tags.ownerName = \"${OWNER}\"" $FILE_PATH
+yq --inplace ".spec.resourceConfig.tags.projectName = \"${PROJECT}\"" $FILE_PATH
